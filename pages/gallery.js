9efb9b01dee6914861f698aaa80834b534ae404d
ParkerMessner <66144673+ParkerMessner@users.noreply.github.com>
2023-02-26 17:21:48 -0500

    Updated gallery

--- pages/gallery.js
+++ pages/gallery.js
@@ -4,32 +4,42 @@ import GalleryDisplay from '../comps/GalleryDisplay';
 export default function Gallery() {
     const images = ['/GO/GO1.jpg','/GO/GO2.jpg'];
     const GlenOaks = [
-        { name: "/GO/GO1.jpg", w: "500px" , h: "640px"},
-        { name: "/GO/GO2.jpg", w: "512px" , h: "640px" },
+        //{ name: "/GO/GO1.jpg", w: "500px" , h: "640px"},
+        //{ name: "/GO/GO2.jpg", w: "512px" , h: "640px" },
+        { name: "/GO/GO3.jpg", w: "480px" , h: "640px" },
+        { name: "/GO/GO5.jpg", w: "489px" , h: "640px" },
+        
       ];
 
     const Onwenstia = [
         { name: "/ONW/ONW1.jpg", w: "512px" , h: "640px"},
         { name: "/ONW/ONW2.jpg", w: "512px" , h: "640px"},
-        { name: "/ONW/ONW3.jpg", w: "640px" , h: "600px"},
-        { name: "/ONW/ONW4.jpg", w: "480px" , h: "640px"},
+        //{ name: "/ONW/ONW3.jpg", w: "640px" , h: "600px"}, Yeti
+        //{ name: "/ONW/ONW4.jpg", w: "480px" , h: "640px"}, Dresser and racks
         { name: "/ONW/ONW5.jpg", w: "480px" , h: "640px"},
         { name: "/ONW/ONW6.jpeg", w: "3024px" , h: "4032px"},
         { name: "/ONW/ONW7.jpeg", w: "2612px" , h: "3679px"},
         { name: "/ONW/ONW8.jpeg", w: "3023px" , h: "3600px"},
 
       ];
+    
+    const SunsetRidge = [//Put at the top of the gallery
+        { name: "/SR/SR1.jpg", w: "480px" , h: "640px"},
+        { name: "/SR/SR3.jpg", w: "509px" , h: "640px"},
+        { name: "/SR/SR4.jpg", w: "480px" , h: "640px"},
+ 
+      ];
 
       const StrawberryCreek = [
         //{ name: "/SC/SC1.jpg", w: "480px" , h: "640px"},
         //{ name: "/SC/SC2.jpg", w: "480px" , h: "640px"},
         { name: "/SC/SC3.jpg", w: "480px" , h: "640px"},
-        { name: "/SC/SC4.jpg", w: "480px" , h: "640px"},
-        { name: "/SC/SC5.jpg", w: "480px" , h: "640px"},
+        //{ name: "/SC/SC4.jpg", w: "480px" , h: "640px"}, Gfore
+        //{ name: "/SC/SC5.jpg", w: "480px" , h: "640px"}, Mistletoe
         { name: "/SC/SC6.jpg", w: "480px" , h: "640px"},
         { name: "/SC/SC7.jpg", w: "480px" , h: "640px"},
         { name: "/SC/SC8.jpg", w: "552px" , h: "640px"},
-        { name: "/SC/SC9.jpg", w: "480px" , h: "640px"},
+        //{ name: "/SC/SC9.jpg", w: "480px" , h: "640px"}, Bears and sign in middle
         { name: "/SC/SC10.jpg", w: "480px" , h: "640px"},
         { name: "/SC/SC11.jpg", w: "480px" , h: "640px"},
         { name: "/SC/SC12.jpg", w: "492px" , h: "640px"},
@@ -55,6 +65,8 @@ export default function Gallery() {
             <div style={{textAlign:"center"}}>
             <h1>Gallery</h1>
             </div>
+
+            <GalleryDisplay imgs = {SunsetRidge} clubName="Sunset Ridge" clubLoc="Northfield, IL"/>
             
             <GalleryDisplay imgs = {StrawberryCreek} clubName="Strawberry Creek" clubLoc="Kenosha, WI"/>
 
